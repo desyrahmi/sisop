3adf28ef0b83bcef64c717409451af10df7aeab3
desyrahmi <desyrahmi09@gmail.com>
2015-11-10 20:15:17 +0700

    update shell

--- shell.c
+++ shell.c
@@ -1,169 +1,74 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<unistd.h>
 #include<string.h>
+#include<stdlib.h>
+#include<signal.h>
 #include<sys/wait.h>
 
-#define BUFSIZE 1024
-#define BUFFSIZE 64
-#define DELIM " \t\r\n\a"
-
-int main(int argc, char **argv){
-	loop();
-	return EXIT_SUCCESS;
-}
-
-void loop(void){
-	char *line;
-	char **arg;
-	int status;
-
-	do{
-		printf("> ");
-		line = read_line();
-		arg = split_line(line);
-		status = execute(arg);
-
-		free(line);
-		free(arg);
-	}
-	while(status);
-}
-
-char *read_line(void){
-	int bufsize = BUFSIZE;
-	int position = 0;
-	char *buffer = malloc(sizeof(char) * bufsize);
-	int c;
-
-	if(!buffer){
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-	while(1){
-		c = getchar();
-		if (c == EOF || c == '\n'){
-			buffer[position] = '\0';
-			return buffer;
+int execute(char** subcommand,int position){
+	pid_t pid;
+	int temp=0;
+	if(strcmp(subcommand[0],"cd")==0){
+		if(subcommand[1]==NULL) fprintf(stderr,"cd: No argument\n");
+		else{
+			if(chdir(subcommand[1])!=0) fprintf(stderr,"cd: No such file or directory\n");
 		}
-		else {
-			buffer[position] = c;
-		}
-		position++;
-
-		if (position >= bufsize){
-			bufsize+= BUFSIZE;
-			buffer = realloc(buffer, bufsize);
-			if (!buffer){
-				fprintf(stderr, "lsh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
-}
-
-char **split_line(char *line){
-	int bufsize = BUFFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
-	char *token;
-
-	if(!tokens){
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILRE);
-	}
-	token = strtok(line, DELIM);
-	while(token != NULL){
-		tokens[position] = token;
-		position++;
-
-		if (position >= bufsize){
-			bufsize += BUFFSIZE;
-			tokens = realloc(tokens, bufsize * sizeof(char*));
-			if(!tokens){
-				fprintf(stderr, "lsh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-		token = strtok(NULL, DELIM);
-	}
-	tokens[position] = NULL;
-	return tokens;
-}
-
-int launch(char **arg){
-	pid_t pid1, pid2;
-	int status;
-	pid1 = fork();
-	if(pid1 == 0){
-		if(execvp(arg[0], arg) == -1){
-			perror("lsh");
-		}
-		exit(EXIT_FAILURE);
-	}
-	else if (pid1 <  0){
-		perror("lsh");
-	}
-	else{
-		do{
-			pid2 = waitpid1(pid1, &status, WUNTRACED);
-		}
-		while(!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-	return 1;
-}
-
-int cd(char **arg);
-int help(char **arg);
-int exit(char **arg);
-char *builtin_str[] = {
-	"cd",
-	"help",
-	"exit"
-};
-int (*builtin_func[]) (char **) = {
-	&cd,
-	&help,
-	&exit
-};
-
-int num_builtin(){
-	return sizeof(builtin_str) / sizeof(char*);
-}
-
-int cd(char **arg){
-	if (arg[1] == NULL) {
-		fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-	}
-	else{
-		if (chdir(arg[1] != 0){
-			perror("lsh");
-		}
-	}
-	return 1;
-}
-
-int help(char **arg){
-	int i;
-	for (i=0; i<num_builtin(); i++){
-		printf(" %s\n", builtin_str[i]);
-	}
-	printf("Use the man command for information on other program\n);
-	return 1;
-}
-
-int exit(char **arg){
-	return 0;
-}
-
-int execute(char **arg){
-	int i;
-	if (arg[0] == NULL) {
 		return 1;
 	}
-	for (i=0; i<num_builtin(); i++){
-		if(strcmp(arg[0], builtin_str[i]) == 0){
-			return (*builtin_func[i](arg);
+	else{
+		if(strcmp(subcommand[position-1],"&")==0) temp=1;
+		pid = fork();
+		if(pid==0){
+			if(temp==1) subcommand[position-1]='\0';
+			execvp(subcommand[0], subcommand);
+			return 1;
+		}
+		else if(pid<0){
+			printf("ERROR\n");
+			return 0;
+		}
+		else{
+			if(temp==1);
+			else{
+				int status;
+				waitpid(pid,&status,0);
+			}
+			return 1;
 		}
 	}
-	return launch(arg);
+}
+
+void sighandler(){
+	char computername[100];
+	char directory[100];
+	fprintf(stderr,"\n%s@%s:~%s$ ",getenv("LOGNAME"),computername,directory);
+}
+
+int main()
+{
+	char command[100];
+	char *subcommand[20];
+	int position=0;
+	char computername[100];
+	char directory[100];
+
+	while(1){
+		gethostname(computername,100);
+		getcwd(directory,100);
+		printf("%s@%s:~%s$ ",getenv("LOGNAME"),computername,directory);
+		signal(SIGINT,sighandler);
+                signal(SIGTSTP,sighandler);
+		if(fgets(command,100,stdin)==NULL){
+			printf("\n");
+			break;
+		}
+		if(command[strlen(command)-1]=='\n') command[strlen(command)-1]='\0';
+		subcommand[position]=strtok(command," ");
+		for(position=1;position<20;position++){
+			subcommand[position] = strtok(NULL," ");
+			if(subcommand[position]==NULL) break;
+		}
+		if(execute(subcommand,position)==0) break;
+	}
+	return 0;
 }
